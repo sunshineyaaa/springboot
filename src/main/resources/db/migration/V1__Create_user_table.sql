ad96dc778ea5eda7b22301c724a6c1e604896009
sunshineyaaa <18862283092@163.com>
2019-07-11 00:27:43 +0800

    flyway

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table USER
-(
-  ID           int AUTO_INCREMENT primary key NOT NULL,
-  NAME         VARCHAR(50),
-  ACCOUNT_ID   VARCHAR(100),
-  TOKEN        CHAR(36),
-  GMT_CREATE   BIGINT,
-  GMT_MODIFIED BIGINT
-);
